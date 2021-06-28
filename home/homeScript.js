a68f57bf0f72e5ec938905676a056b1fc0761552
Michael Teshome <michaelteshome1626@gmail.com>
2021-06-28 18:51:51 -0400

    adjusted size of display + removed unused vinyls

--- home/homeScript.js
+++ home/homeScript.js
@@ -2,18 +2,15 @@ var slideIndex = 0;
 var vinyls = [
     ["resources/damn-crop.png", "DAMN.", "Kendrick Lamar", 99.7 ],
     ["resources/flower-bloy-crop.png", "Flower Boy", "Tyler, the Creator", 100 ],
-    //["resources/things-fall-apart-crop.png", "Things Fall Apart", "The Roots", 85.5],
-    //["resources/illmatic-crop.png", "Illmatic", "Nas", 89.8],
-    //["resources/crtl-crop.png.jpg", "Ctrl", "SZA", 100 ],
-    ["resources/acid-rap-crop.png", "Acid Rap", "Chance the Rapper", 100],
-    ["resources/good-girl-gone-bad-crop.png", "Good Girl Gone Bad", "Rihanna", 100],
-    ["resources/late-registration-crop.png", "Late Registration", "Kanye West", 100],
-    //["resources/lemonade-crop.png", "Lemonade", "Beyonce", 100],
-    ["resources/man-on-the-moon-crop.png", "Man on the Moon", "Kid Cudi", 100],
-    ["resources/the-blueprint-crop.png", "The Blueprint", "Jay Z", 100],
-    ["resources/the-never-story-crop.png", "The Never Story", "JID", 100],
-    ["resources/the-off-season-crop.png", "The Off Season", "J. Cole", 100],
-    //["resources/the-slim-shady-lp-crop.jpg", "The Slim Shady LP", "Eminem", 100]
+    ["resources/lailas-wisdom-crop.png", "Laila's Wisdom", "Rapsody", 87.7],
+    ["resources/how-i-got-over-crop.png", "how i got over", "The Roots", 90.2],
+    ["resources/ctrl-crop.png", "Ctrl", "SZA", 97],
+    ["resources/acid-rap-crop.png", "Acid Rap", "Chance the Rapper", 97.4],
+    ["resources/good-girl-gone-bad-crop.png", "Good Girl Gone Bad", "Rihanna", 98.8],
+    ["resources/late-registration-crop.png", "Late Registration", "Kanye West", 99],
+    ["resources/man-on-the-moon-3-crop.png", "Man on the Moon 3", "Kid Cudi", 88.5],
+    ["resources/the-never-story-crop.png", "The Never Story", "JID", 90.75],
+    ["resources/the-off-season-crop.png", "The Off Season", "J. Cole", 91.8]
 ];
 // showRecords();
 newDisplay();
@@ -42,6 +39,9 @@ function newDisplay(){
         slideIndex = 0;
     }
     records[0].style.display = "flex";
+    if (vinyls[slideIndex][3]<100){
+        space[0].style.alignSelf = "flex-start";
+    }
     space[0].src = vinyls[slideIndex][0];
     space[0].style.width = vinyls[slideIndex][3] + "%";
     name[0].innerHTML = vinyls[slideIndex][1];
