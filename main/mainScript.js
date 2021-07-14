f7827184029d37970433ebc6cc25a88ce4006c69
Michael Teshome <michaelteshome1626@gmail.com>
2021-07-14 16:03:47 -0400

    created another scollable display for pop albums
    scorllbar no longer visible on the display

--- main/mainScript.js
+++ main/mainScript.js
@@ -1,14 +1,35 @@
 var vinyls = [
-    ["resources/all-eyez-on-me.jpg", "All Eyez On Me", "2 Pac"],
-    ["resources/eve.jpeg", "Eve", "Rapsody"],
-    ["resources/forest-hills-drive.jpg", "2014 Forest Hiils Drive", "J. Cole"],
-    ["resources/good-kid-maad-city.jpg", "Good Kid Maad City", "Kendrick Lamar"],
-    ["resources/man-on-the-moon.jpg", "Man on the Moon", "Kid Cudi"],
-    ["resources/my-beautiful-dark-twisted.jpg", "My Beautiful Dark Twisted Fantasy", "Kanye West"],
-    ["resources/take-care.jpg", "Take Care", "Drake"],
-    ["resources/the-blueprint.jpg", "The Blueprint ", "Jay Z"],
-    ["resources/the-pinkprint.jpg", "The Pinkprint", "Nicki Minaj"]
+    ["resources/hh/all-eyez-on-me.jpg", "All Eyez On Me", "2 Pac"],
+    ["resources/hh/eve.jpeg", "Eve", "Rapsody"],
+    ["resources/hh/forest-hills-drive.jpg", "2014 Forest Hills Drive", "J. Cole"],
+    ["resources/hh/good-kid-maad-city.jpg", "Good Kid Maad City", "Kendrick Lamar"],
+    ["resources/hh/man-on-the-moon.jpg", "Man on the Moon", "Kid Cudi"],
+    ["resources/hh/my-beautiful-dark-twisted.png", "My Beautiful Dark Twisted Fantasy", "Kanye West"],
+    ["resources/hh/take-care.jpg", "Take Care", "Drake"],
+    ["resources/hh/the-blueprint.jpg", "The Blueprint ", "Jay Z"],
+    ["resources/hh/the-pinkprint.jpg", "The Pinkprint", "Nicki Minaj"],
+    ["resources/pop/1989.png", "1989", "Taylor Swift"],
+    ["resources/pop/fine-line.png", "Fine Line", "Harry Styles"],
+    ["resources/pop/future-sex-love-sounds.jpg", "Future Sex/Love Sounds", "Justin Timberlake"],
+    ["resources/pop/melodrama.jpg", "Melodrama", "Lorde"],
+    ["resources/pop/off-the-wall.jpg", "Off the Wall", "Michael Jackson"],
+    ["resources/pop/purple-rain.jpg", "Purple Rain", "Prince"],
+    ["resources/pop/sweetner.jpg", "Sweetener", "Ariana Granade"],
+    ["resources/pop/when-we-all-fall-asleep.jpg", "When We All Fall Asleep, Where do We Go", "Billie Eilish"],
+    ["resources/pop/whitney.jpg", "Whitney", "Whitney Houston"]
 ];
+
+// var popVinyls = [
+//     ["resources/pop/1989.png", "1989", "Taylor Swift"],
+//     ["resources/pop/fine-line.png", "Fine Line", "Harry Styles"],
+//     ["resources/pop/future-sex-love-sounds.jpg", "Future Sex/Love Sounds", "Justin Timberlake"],
+//     ["resources/pop/melodrama.jpg", "Melodrama", "Lorde"],
+//     ["resources/pop/off-the-wall.jpg", "Off the Wall", "Michael Jackson"],
+//     ["resources/pop/purple-rain.jpg", "Purple Rain", "Prince"],
+//     ["resources/pop/sweetner.jpg", "Sweetener", "Ariana Granade"],
+//     ["resources/pop/when-we-all-fall-asleep.jpg", "When We All Fall Asleep, Where do We Go", "Billie Eilish"],
+//     ["resources/pop/whitney.jpg", "Whitney", "Whitney Houston"]
+// ];
 
 hhDisplay();
 
